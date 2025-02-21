82f19c682788583913caf266cb1d43a2106560d3
Rean18 <37306114+Rean18@users.noreply.github.com>
2025-02-21 10:56:41 +0100

    Gestion de l'affichage des matches en fonction du nombre de matches pouvant être joués en simultané

--- src/pages/MatchesDashboard.jsx
+++ src/pages/MatchesDashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { roundRobin } from '../utils/matchLogic.js';
 
 export function MatchesDashboard() {
@@ -8,29 +8,59 @@ export function MatchesDashboard() {
   const rulesJSON = localStorage.getItem('formData');
   const rules = JSON.parse(rulesJSON || '{}');
   const twoLeggedTie = rules.twoLeggedTie === 'oui';
-  const numberOfSimultMatche = rules.numberOfSimultMatche;
   const victoryPoints = Number(rules.victoryPoint);
   const defeatPoints = Number(rules.defeatPoint);
   const drawPoints = Number(rules.drawPoint);
+  const numberOfSimultMatches = Number(rules.simultMatches);
 
   const [teamsStats, setTeamsStats] = useState(parsedTeams);
   const [inputValues, setInputValues] = useState(() => {
     const storedInputs = localStorage.getItem('inputValues');
     return storedInputs ? JSON.parse(storedInputs) : {};
   });
+  const [chunks, setChunks] = useState([]);
 
-  const matches = roundRobin(teamsList, twoLeggedTie);
+  const matches = useMemo(
+    () => roundRobin(teamsList, twoLeggedTie),
+    [teamsList, twoLeggedTie]
+  );
   console.log('liste des matches JSX:', matches);
 
   const handleStats = (e) => {
     // Gestion des Inputs
     const { id, value } = e.target;
-    setInputValues((prevValues) => {
-      const newValues = { ...prevValues, [id]: value };
-      localStorage.setItem('inputValues', JSON.stringify(newValues));
-      return newValues;
-    });
+    if (value === '') {
+      setInputValues((prevValues) => {
+        const newValues = { ...prevValues, [id]: '' };
+        localStorage.setItem('inputValues', JSON.stringify(newValues));
+        return newValues;
+      });
+    } else {
+      setInputValues((prevValues) => {
+        const newValues = { ...prevValues, [id]: value };
+        localStorage.setItem('inputValues', JSON.stringify(newValues));
+        return newValues;
+      });
+    }
   };
+
+  // Gestion de la l'affihage des matches
+  useEffect(() => {
+    console.log(
+      'useEffect chunking déclenché',
+      numberOfSimultMatches,
+      matches.length
+    );
+    const newChunks = [];
+    for (let i = 0; i < matches.length; i += numberOfSimultMatches) {
+      const chunk = matches.slice(i, i + numberOfSimultMatches);
+      newChunks.push(chunk);
+    }
+    setChunks(newChunks);
+    console.log('Matches par vrais tour', newChunks);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [numberOfSimultMatches]);
 
   // Mise à jours des Stats
   useEffect(() => {
@@ -38,34 +68,45 @@ export function MatchesDashboard() {
       let newGoalsScored = { ...(team.goalsScored || {}) };
       let newGoalsConceded = { ...(team.goalsConceded || {}) };
       let newPoints = { ...(team.points || {}) };
-      console.log('POINTS AVANTS', newPoints);
 
-      matches.forEach((match, index) => {
-        if (match.includes(team.name)) {
-          const teamInputId = `${index}-${team.name}`;
-          const opponent = match[0] === team.name ? match[1] : match[0];
-          const opponentInputId = `${index}-${opponent}`;
+      chunks.forEach((chunk, chunkIndex) => {
+        chunk.forEach((match, matchIndex) => {
+          if (match.includes(team.name)) {
+            const teamInputId = `${chunkIndex}-${matchIndex}-${team.name}`;
+            const opponent = match[0] === team.name ? match[1] : match[0];
+            const opponentInputId = `${chunkIndex}-${matchIndex}-${opponent}`;
 
-          if (inputValues[teamInputId] !== undefined) {
-            newGoalsScored[teamInputId] = Number(inputValues[teamInputId]);
-          }
-          if (inputValues[opponentInputId] !== undefined) {
-            newGoalsConceded[teamInputId] = Number(
-              inputValues[opponentInputId]
-            );
-          }
+            if (inputValues[teamInputId] !== undefined) {
+              newGoalsScored[teamInputId] = Number(inputValues[teamInputId]);
+            }
+            if (inputValues[opponentInputId] !== undefined) {
+              newGoalsConceded[teamInputId] = Number(
+                inputValues[opponentInputId]
+              );
+            }
+            if (
+              inputValues[teamInputId] === undefined ||
+              inputValues[teamInputId] === ''
+            ) {
+              newPoints[teamInputId] = 0;
+              newGoalsConceded[teamInputId] = 0;
+              newGoalsScored[teamInputId] = 0;
+            } else {
+              if (newGoalsScored[teamInputId] < newGoalsConceded[teamInputId]) {
+                newPoints[teamInputId] = defeatPoints;
+              }
+              if (
+                newGoalsScored[teamInputId] === newGoalsConceded[teamInputId]
+              ) {
+                newPoints[teamInputId] = drawPoints;
+              }
 
-          if (newGoalsScored[teamInputId] < newGoalsConceded[teamInputId]) {
-            newPoints[teamInputId] = defeatPoints;
+              if (newGoalsScored[teamInputId] > newGoalsConceded[teamInputId]) {
+                newPoints[teamInputId] = victoryPoints;
+              }
+            }
           }
-          if (newGoalsScored[teamInputId] === newGoalsConceded[teamInputId]) {
-            newPoints[teamInputId] = drawPoints;
-          }
-
-          if (newGoalsScored[teamInputId] > newGoalsConceded[teamInputId]) {
-            newPoints[teamInputId] = victoryPoints;
-          }
-        }
+        });
       });
       const totalGoalsScored = Object.values(team.goalsScored).reduce(
         (acc, curr) => acc + curr,
@@ -93,37 +134,37 @@ export function MatchesDashboard() {
 
     setTeamsStats(updatedStats);
     localStorage.setItem('teamsData', JSON.stringify(updatedStats));
-  }, [
-    inputValues,
-    matches,
-    defeatPoints,
-    drawPoints,
-    victoryPoints,
-    teamsStats,
-  ]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValues, victoryPoints, defeatPoints, drawPoints, teamsData]);
 
   return (
     <div>
       <h2>Liste de tous les matchs</h2>
-      {matches.map((team, index) => (
-        <div key={index}>
-          {team[0]}
-          <input
-            id={index + '-' + team[0]}
-            name={team[0]}
-            type="number"
-            onChange={handleStats}
-            value={inputValues[index + '-' + team[0]]}
-          />{' '}
-          -{' '}
-          <input
-            id={index + '-' + team[1]}
-            name={team[1]}
-            type="number"
-            onChange={handleStats}
-            value={inputValues[index + '-' + team[1]]}
-          />
-          {team[1]}
+
+      {chunks.map((chunk, chunkIndex) => (
+        <div key={chunkIndex}>
+          <h2>Tour {chunkIndex + 1}</h2>
+          {chunk.map((team, matchIndex) => (
+            <div key={matchIndex}>
+              {team[0]}
+              <input
+                id={`${chunkIndex}-${matchIndex}-${team[0]}`}
+                name={team[0][0]}
+                type="number"
+                onChange={handleStats}
+                value={inputValues[`${chunkIndex}-${matchIndex}-${team[0]}`]}
+              />{' '}
+              -{' '}
+              <input
+                id={`${chunkIndex}-${matchIndex}-${team[1]}`}
+                name={team[1]}
+                type="number"
+                onChange={handleStats}
+                value={inputValues[`${chunkIndex}-${matchIndex}-${team[1]}`]}
+              />
+              {team[1]}
+            </div>
+          ))}
         </div>
       ))}
     </div>
